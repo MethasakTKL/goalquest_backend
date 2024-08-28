9b3eedd630e16cfaf5c3d445b635f3fbbee516e9
Natthawut Suksai <owen55660@gmail.com>
2024-08-28 22:36:32 +0700

    fix code

--- goalquest_backend/routers/__init__.py
+++ goalquest_backend/routers/__init__.py
@@ -6,10 +6,4 @@ def init_router(app):
     app.include_router(points.router)
     app.include_router(goals.router)
     app.include_router(tasks.router)
-<<<<<<< HEAD
     app.include_router(done_tasks.router)
-=======
-
-    app.include_router(rewards.router)
-    app.include_router(redeems.router)
->>>>>>> 73166a998d08edbb244570c861981c628393d8c2
