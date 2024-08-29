1f12e1f009fda66073d130aa43dac64e16d15a98
Natthawut Suksai <owen55660@gmail.com>
2024-08-29 15:24:01 +0700

    add Authen for get points

--- goalquest_backend/routers/points.py
+++ goalquest_backend/routers/points.py
@@ -4,51 +4,62 @@ from sqlmodel import select
 from goalquest_backend.models import get_session
 from goalquest_backend.models.points import Point, BasePoint
 from goalquest_backend.models.users import DBUser
+from typing import Annotated
+
+from .. import deps
+from .. import models
 
 router = APIRouter(
     prefix="/points",
     tags=["Points"]
 )
 
-@router.post("/", response_model=Point)
-async def create_point(point: BasePoint, session: AsyncSession = Depends(get_session)):
-    user = await session.get(DBUser, point.user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+# @router.post("/", response_model=Point)
+# async def create_point(point: BasePoint, session: AsyncSession = Depends(get_session)):
+#     user = await session.get(DBUser, point.user_id)
+#     if not user:
+#         raise HTTPException(status_code=404, detail="User not found")
 
-    db_point = Point(**point.dict())
-    session.add(db_point)
-    await session.commit()
-    await session.refresh(db_point)
-    return db_point
+#     db_point = Point(**point.dict())
+#     session.add(db_point)
+#     await session.commit()
+#     await session.refresh(db_point)
+#     return db_point
 
 @router.get("/{point_id}", response_model=Point)
-async def read_point(point_id: int, session: AsyncSession = Depends(get_session)):
+async def read_point(
+    point_id: int, 
+    session: Annotated[AsyncSession, Depends(get_session)],
+    current_user: Annotated[models.User, Depends(deps.get_current_user)],
+    ) -> Point:
     point = await session.get(Point, point_id)
     if not point:
         raise HTTPException(status_code=404, detail="Point not found")
+    if point.user_id != current_user.id:
+        raise HTTPException(status_code=403, detail="Not authorized to access this goal")
     return point
 
-@router.put("/{point_id}", response_model=Point)
-async def update_point(point_id: int, point_update: BasePoint, session: AsyncSession = Depends(get_session)):
-    point = await session.get(Point, point_id)
-    if not point:
-        raise HTTPException(status_code=404, detail="Point not found")
 
-    point.total_point = point_update.total_point
-    point.last_earned_at = point_update.last_earned_at
+# @router.put("/{point_id}", response_model=Point)
+# async def update_point(point_id: int, point_update: BasePoint, session: AsyncSession = Depends(get_session)):
+#     point = await session.get(Point, point_id)
+#     if not point:
+#         raise HTTPException(status_code=404, detail="Point not found")
 
-    session.add(point)
-    await session.commit()
-    await session.refresh(point)
-    return point
+#     point.total_point = point_update.total_point
+#     point.last_earned_at = point_update.last_earned_at
 
-@router.delete("/{point_id}", response_model=dict)
-async def delete_point(point_id: int, session: AsyncSession = Depends(get_session)):
-    point = await session.get(Point, point_id)
-    if not point:
-        raise HTTPException(status_code=404, detail="Point not found")
+#     session.add(point)
+#     await session.commit()
+#     await session.refresh(point)
+#     return point
 
-    await session.delete(point)
-    await session.commit()
-    return {"detail": "Point deleted"}
+# @router.delete("/{point_id}", response_model=dict)
+# async def delete_point(point_id: int, session: AsyncSession = Depends(get_session)):
+#     point = await session.get(Point, point_id)
+#     if not point:
+#         raise HTTPException(status_code=404, detail="Point not found")
+
+#     await session.delete(point)
+#     await session.commit()
+#     return {"detail": "Point deleted"}
